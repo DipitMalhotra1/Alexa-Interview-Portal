8ac5514102b8dd7b37d010aab3aed9452385575e
Dipit Malhotra <dipitmalhotra@gmail.com>
2017-05-09 14:41:36 -0500

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,247 @@
+var candidates = {
+    "Harry" : {
+        "Interview_Date": "May 8th 2017",
+        "Status": "Completed"
+    }, 
+    "Max" : {
+        "Interview_Date": "June 9th 2017",
+        "Status": "Incomplete"
+    }, 
+    "John" : {
+        "Interview_Date": "Aug 18th 2017",
+        "Status": "Inprocess"
+    }, 
+    "Alex" : {
+        "Interview_Date": "Jan 28th 2017",
+        "Status": "Completed"
+    }, 
+    "Tommy" : {
+        "Interview_Date": "May 18th 2017",
+        "Status": "Incomplete"
+    }, 
+    "Oliver" : {
+        "Interview_Date": "Feb 28th 2017",
+        "Status": "Incomplete"
+    }, 
+    "David" : {
+        "Interview_Date": "Apr 1st 2017",
+        "Status": "Inprocess"
+    }
+}
+
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+
+    if (event.session.application.applicationId !== "amzn1.ask.skill.201b6e8d-b2b6-4fa9-9508-e4a7b454c573") {
+        context.fail("Invalid Application ID");
+     }
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    // add any session init logic here
+}
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    getWelcomeResponse(callback)
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+
+    var intent = intentRequest.intent
+    var intentName = intentRequest.intent.name;
+
+    // dispatch custom intents to handlers here
+    if (intentName == "ReindeerIntent") {
+        handleReindeerResponse(intent, session, callback)
+    } else if (intentName == "AMAZON.YesIntent") {
+        handleYesResponse(intent, session, callback)
+    } else if (intentName == "AMAZON.NoIntent") {
+        handleNoResponse(intent, session, callback)
+    } else if (intentName == "AMAZON.HelpIntent") {
+        handleGetHelpRequest(intent, session, callback)
+    } else if (intentName == "AMAZON.StopIntent") {
+        handleFinishSessionRequest(intent, session, callback)
+    } else if (intentName == "AMAZON.CancelIntent") {
+        handleFinishSessionRequest(intent, session, callback)
+    } else {
+        throw "Invalid intent"
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+
+}
+
+// ------- Skill specific logic -------
+
+function getWelcomeResponse(callback) {
+    var speechOutput = "Welcome to Filetered Candidate Portal! I can give you the details of all the candidates: "+
+    "I can only give candidate information at a time. Which candidate  are you interested in?"
+
+    var reprompt = "Which candidate are you interested in? "
+    var header = "Candidate Details!"
+
+    var shouldEndSession = false
+
+    var sessionAttributes = {
+        "speechOutput" : speechOutput,
+        "repromptText" : reprompt
+    }
+
+    callback(sessionAttributes, buildSpeechletResponse(header, speechOutput, reprompt, shouldEndSession))
+
+}
+
+function handleReindeerResponse(intent, session, callback) {
+    var candidate = intent.slots.candidates.value.toLowerCase()
+
+    if (!candidates[candidate]) {
+        var speechOutput = "Candidate not found. Ask again!"
+        var repromptText = "Try asking about some other candidate"
+        var header = "Not Found"
+    } else {
+        var Interview_Date = candidates[candidate].Interview_Date
+        var status = candidates[candidate].status
+        var speechOutput = capitalizeFirst(candidate) + " " + Interview_Date + " and " + status + ". Do you want to hear about more candidates?"
+        var repromptText = "Do you want to hear about more candidates?"
+        var header = capitalizeFirst(candidate)
+    }
+
+    var shouldEndSession = false
+
+    callback(session.attributes, buildSpeechletResponse(header, speechOutput, repromptText, shouldEndSession))
+}
+
+function handleYesResponse(intent, session, callback) {
+    var speechOutput = "Great! Which candidate are you looking for?"
+    var repromptText = speechOutput
+    var shouldEndSession = false
+
+    callback(session.attributes, buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession))
+}
+
+function handleNoResponse(intent, session, callback) {
+    handleFinishSessionRequest(intent, session, callback)
+}
+
+function handleGetHelpRequest(intent, session, callback) {
+    // Ensure that session.attributes has been initialized
+    if (!session.attributes) {
+        session.attributes = {};
+    }
+
+    var speechOutput = "I can give you the details of the candidates in the portal: " +
+    " Which candidate are you interested in? Remember, I can only give details about one candidate at a time."
+
+    var repromptText = speechOutput
+
+    var shouldEndSession = false
+
+    callback(session.attributes, buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession))
+
+}
+
+function handleFinishSessionRequest(intent, session, callback) {
+    // End the session with a "Good bye!" if the user wants to quit the game
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Good bye! Thank you for using Filtered App!", "", true));
+}
+
+
+// ------- Helper functions to build responses for Alexa -------
+
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
+
+function capitalizeFirst(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
